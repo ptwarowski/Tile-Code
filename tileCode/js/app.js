0da31f9fbdd8d11cc70eee3ebd6446841c651cef
Przemek Twarowski <przemektwarowski@gmail.com>
2021-02-21 00:58:53 +0100

    ver 0.1.1 Plan Draw + undoBtn

--- tileCode/js/app.js
+++ tileCode/js/app.js
@@ -1,16 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import ReactDom from "react-dom";
-import Canvas from "./canvas";
+
 
 let xCount = 0;
 let yCount =0;
 let xOffset = 0;
 let yOffset =0;
+let svgFill = 'none';
+let svgStroke = 1;
 
 function App() {
-  const [plan, setPlan] = useState('M0 0');
-  const [co_ordinates, setCo_ordinates] = useState([]);
-
+  const [plan, setPlan] = useState(['M 0 0']);
+  const svgViewBox=`${xOffset-10} ${yOffset-10} 800 400`;
+  
+  
   const handleChange = () => {
     const $wallLenght = document.getElementById("wallLenght");
     
@@ -20,50 +23,55 @@ function App() {
     if (event.target.name==="buttonRight") {event.preventDefault(); xCount += parseInt($wallLenght.value)};
     if (xOffset > xCount) {xOffset=xCount};
     if (yOffset > yCount) {yOffset=yCount};
-    setPlan((prevState)=>prevState + ` L${xCount} ${yCount}`)
+    setPlan((prevState)=>[...prevState, ` L ${xCount} ${yCount}`])
     
   }
   
   const handleUndo = () => {
-    let lastIndex = plan.lastIndexOf('L');
-    console.log(lastIndex);
-    setPlan((prevState)=>prevState.slice(-lastIndex));
+    plan.pop();
+    let lastItem = plan[plan.length-1];
+    xCount=parseInt(lastItem.split(' ')[2]);
+    yCount=parseInt(lastItem.split(' ')[3]);
+    setPlan((prevState)=>[...prevState]);
   };
 
   const handleDone = () => {
-    setPlan((prevState)=>prevState + ` Z`)
+    setPlan((prevState)=>prevState + ` Z`);
+    svgFill = "gainsboro";
+    svgStroke = 3;
   };
 
   console.log(plan);
-
-  const draw = (ctx) => {
-    ctx.save();
-    ctx.translate(30,30);
-    let floorPath= new Path2D(plan);
-    ctx.strokeStyle = 'black';
-    ctx.fillStyle="gainsboro";
-    floorPath.lineWidth = 5;
-    ctx.stroke(floorPath);
-    ctx.restore();
-    // ctx.fill() 
-  }
   
   return (
     <>
     
-    <svg className="planSVG" viewBox={`${xOffset-10} ${yOffset-10} 800 400`} style={{padding:20, width:800}} ><path  d={plan}  stroke="black"
-    strokeWidth="3" fill="none" /></svg>
+    <svg className="planSVG" viewBox={svgViewBox} style={{padding:20, width:800}} >
+      <path  d={plan}  stroke="black" strokeWidth={svgStroke} fill={svgFill} />
+    </svg>
     <br></br>
-    <button type="submit" name= "buttonUp" className = "upPlanButton" onClick={handleChange}>{"^"}</button>
+    <button type="submit" name= "buttonUp" className = "upPlanButton" onClick={handleChange}>
+      {"^"}
+    </button>
     <br></br>
-    <button type="submit" name= "buttonLeft" className = "leftPlanButton" onClick={handleChange}>{"<"}</button>
+    <button type="submit" name= "buttonLeft" className = "leftPlanButton" onClick={handleChange}>
+      {"<"}
+    </button>
     <input id="wallLenght" type="number" placeholder="podaj długość"></input>
-    <button type="submit" name= "buttonRight" className = "rightPlanButton" onClick={handleChange}>{">"}</button>
+    <button type="submit" name= "buttonRight" className = "rightPlanButton" onClick={handleChange}>
+      {">"}
+    </button>
     <br></br>
-    <button type="submit" name= "buttonDown" className = "downPlanButton" onClick={handleChange}>{"v"}</button>
+    <button type="submit" name= "buttonDown" className = "downPlanButton" onClick={handleChange}>
+      {"v"}
+    </button>
     <br></br>
-    <button type="submit" name= "undo" className = "undoButton" onClick={()=>handleUndo}>undo</button>
-    <button name= "closePlan" className = "closePlanButton" onClick={handleDone}>done</button>
+    <button type="submit" name= "undo" className = "undoButton" onClick={handleUndo}>
+      undo
+    </button>
+    <button name= "closePlan" className = "closePlanButton" onClick={handleDone}>
+      done
+    </button>
     </>)
 }
 
